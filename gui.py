4a1c3d319775b0c177f096e2ae99c3a0ccedef76
Kayla <kaylavincent14@gmail.com>
2024-03-01 15:17:47 -0500

    Implemented the Edit button day 17

--- gui.py
+++ gui.py
@@ -4,9 +4,12 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter the to-do", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])  # key to identify each value
+edit_button = sg.Button("Edit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 10))  # has to pass widget types ie label,window, etc. Each list represents a row
 while True:
     event, values = window.read()
@@ -18,6 +21,19 @@ while True:
             new_todo = values['todo'] + "\n"  # gets the value of the key="todo"
             todos.append(new_todo.title())
             functions.write_todos(todos)
+            window['todos'].update(values=todos) # when an item is added update the window in real time
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]  # write 0 to only get the action not the []s or ''s
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo.title()
+            functions.write_todos(todos)
+            window['todos'].update(values=todos) # when an item is added update the window in real time
+
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
 
         case sg.WIN_CLOSED:
             break
